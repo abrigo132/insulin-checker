5f8f8dbbc3aca1abc625a72b8d5ccd97c88a28ef
abrigo132 <garipotter97@gmail.com>
2025-02-18 16:52:02 +0500

    create base settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import BaseModel, PostgresDsn
 from pathlib import Path
 
@@ -7,3 +7,17 @@ BASE_DIR = Path(__file__).parent.parent
 
 class DbConfig(BaseModel):
     url: PostgresDsn
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env.dev",
+        env_nested_delimiter="__",
+        case_sensitive=False,
+        env_prefix="APP_CONFIG__",
+    )
+
+    db: DbConfig
+
+
+settings: DbConfig = DbConfig()
