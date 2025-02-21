3bd15c046056546c6b4559da168a93d6f6468e17
abrigo132 <garipotter97@gmail.com>
2025-02-21 10:54:49 +0500

    realize singleton pattern

--- src/core/models/db_helper.py
+++ src/core/models/db_helper.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
     AsyncEngine,
@@ -9,27 +11,44 @@ from src.core.config import settings
 
 
 class DbHelper:
-    def __init__(
-        self, url: str, echo: bool, echo_pool: bool, max_overflow: int, pool_size: int
-    ):
-        self.async_engine: AsyncEngine = create_async_engine(
-            url=url,
-            echo=echo,
-            echo_pool=echo_pool,
-            max_overflow=max_overflow,
-            pool_size=pool_size,
-        )
-        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
-            bind=self.async_engine,
-            autoflush=False,
-            autocommit=False,
-            expire_on_commit=False,
-        )
+    __instance = None
 
-    async def dispose(self):
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __del__(self):
+        DbHelper.__instance = None
+
+    def __init__(
+        self,
+        url: str,
+        echo: bool,
+        echo_pool: bool,
+        max_overflow: int,
+        pool_size: int,
+    ) -> None:
+        if not hasattr(self, "_initialized"):
+            self.async_engine: AsyncEngine = create_async_engine(
+                url=url,
+                echo=echo,
+                echo_pool=echo_pool,
+                max_overflow=max_overflow,
+                pool_size=pool_size,
+            )
+            self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+                bind=self.async_engine,
+                autoflush=False,
+                autocommit=False,
+                expire_on_commit=False,
+            )
+            self._initialized: bool = True
+
+    async def dispose(self) -> None:
         await self.async_engine.dispose()
 
-    async def session_getter(self):
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
         async with self.session_factory() as session:
             yield session
 
