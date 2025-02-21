c8c42f2809a428d87e146dec26906e3f394bf38c
abrigo132 <garipotter97@gmail.com>
2025-02-21 11:41:00 +0500

    create base model

---
+++ src/core/models/base.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy import MetaData
+
+from src.core.config import settings
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convection)
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
