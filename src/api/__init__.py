3608a8abeb097f5c87f27bb462aef53ad0e300ec
abrigo132 <garipotter97@gmail.com>
2025-02-24 15:12:48 +0500

    add v1 router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
 from src.core.config import settings
+from .v1 import router as v1_router
 
 router = APIRouter(prefix=settings.api.prefix, tags=["api"])
+
+router.include_router(v1_router)
