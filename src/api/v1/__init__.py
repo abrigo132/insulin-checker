db79b333319257de6ebfeb78ba4d1a1a41eecd3f
abrigo132 <garipotter97@gmail.com>
2025-02-25 16:31:39 +0500

    change tags for router

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from src.core.config import settings
 from .users import router as user_router
 
-router = APIRouter(prefix=settings.api.v1.prefix, tags=["v1"])
+router = APIRouter(prefix=settings.api.v1.prefix, tags=["api"])
 
 router.include_router(user_router)
