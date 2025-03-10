7f62899fe64fd4bcd8d462f104587f3757dc1381
abrigo132 <garipotter97@gmail.com>
2025-03-10 14:52:27 +0500

    change import settings

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from src.core.config import settings
+from core import settings
 from .users import router as user_router
 
 router = APIRouter(prefix=settings.api.v1.prefix, tags=["api"])
