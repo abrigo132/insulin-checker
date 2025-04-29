212d351344a56cc377fb2d613df4b64893f1d17f
abrigo132 <garipotter97@gmail.com>
2025-04-29 16:59:25 +0500

    create endpoint get product by username

--- src/api/v1/insulin.py
+++ src/api/v1/insulin.py
@@ -8,3 +8,12 @@ from core.models import db_helper
 
 
 router = APIRouter(prefix=settings.api.v1.insulin, tags=["insulin"])
+
+
+@router.get("/products/{{name}}/")
+async def get_product_by_name(
+    request: Request,
+    name: str,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    pass
