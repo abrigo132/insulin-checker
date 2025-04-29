0e3c2c7600e8f1e4e007b5cf667412297d3789ff
abrigo132 <garipotter97@gmail.com>
2025-04-29 16:59:04 +0500

    add imports

--- src/api/v1/insulin.py
+++ src/api/v1/insulin.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends
+from typing import Annotated
+
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from core import settings
+from core.models import db_helper
 
 
 router = APIRouter(prefix=settings.api.v1.insulin, tags=["insulin"])
