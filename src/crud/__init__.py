5bcba1a7d9cf9d22bc462b87f7db067e06ee4229
abrigo132 <garipotter97@gmail.com>
2025-04-30 15:39:23 +0500

    add Product repository for export

--- src/crud/__init__.py
+++ src/crud/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "UsersCrud",
     "VerifToken",
+    "ProductRepository",
 )
 
 from .users import UsersCrud
 from .verif_token import VerifToken
+from .products import ProductRepository
