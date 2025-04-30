0269ce5e9bed0aeaf042c242e4f1e01cdc3c0d2a
abrigo132 <garipotter97@gmail.com>
2025-04-30 15:38:49 +0500

    add ProductList schema + InsulinDose schema for export

--- src/core/schemas/__init__.py
+++ src/core/schemas/__init__.py
@@ -8,8 +8,10 @@ __all__ = (
     "TokenInfo",
     "ProductInfo",
     "ProductCreate",
+    "ProductList",
+    "InsulinDose",
 )
 
 from .users import User, UserToken, UserRead, UserLogin, UserCreate
 from .jwt import Token, TokenInfo
-from .products import ProductInfo, ProductCreate
+from .products import ProductInfo, ProductCreate, ProductList, InsulinDose
--- src/crud/products.py
+++ src/crud/products.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, ScalarResult
+from sqlalchemy import select, ScalarResult, Sequence
 
 from core.models import Food
 from core.schemas import ProductCreate
@@ -10,8 +10,8 @@ class ProductRepository:
         self.session = session
         self.model = Food
 
-    async def get(self, product_name: str) -> Food | None:
-        stmt = select(self.model).where(self.model.name == product_name)
+    async def get(self, product_id: str) -> Food | None:
+        stmt = select(self.model).where(self.model.name == product_id)
         result: ScalarResult[Food] = await self.session.scalars(statement=stmt)
         return result.one_or_none()
 
@@ -20,3 +20,8 @@ class ProductRepository:
         self.session.add(product)
         await self.session.commit()
         return product
+
+    async def get_list(self, product_id_list: list[int]) -> Sequence[Food]:
+        stmt = select(self.model).where(self.model.id.in_(product_id_list))
+        products: ScalarResult[Food] = await self.session.scalars(statement=stmt)
+        return products.all()
