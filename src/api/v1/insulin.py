4c4756eb5f47259c4ab129627e11d4c7b0b23caf
abrigo132 <garipotter97@gmail.com>
2025-04-30 15:39:46 +0500

    create endpoint calculate insulin dose

--- src/api/v1/insulin.py
+++ src/api/v1/insulin.py
@@ -10,10 +10,13 @@ from core.models import db_helper
 router = APIRouter(prefix=settings.api.v1.insulin, tags=["insulin"])
 
 
-@router.get("/products/{{name}}/")
+@router.get("/products/insulin-dose/{{products}}/", response_model=InsulinDose)
 async def get_product_by_name(
     request: Request,
-    name: str,
+    products: ProductList,
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
-    pass
+    insulin_dose: dict[str, str] = ProductsService(
+        session=session
+    ).calculate_insulin_dose()
+    return InsulinDose(**insulin_dose)
