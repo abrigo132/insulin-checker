8d2e044000e6a16f1e9aa35647aedd7f692c434c
abrigo132 <garipotter97@gmail.com>
2025-05-26 10:53:55 +0500

    add product service from fastapi DI

--- src/api/v1/insulin.py
+++ src/api/v1/insulin.py
@@ -1,10 +1,7 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends, HTTPException
 from typing import Annotated
 
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from core import settings
-from core.models import db_helper
 from core.schemas import ProductList, InsulinDose, ProductCreate, ProductInfo
 from core.services import ProductsService
 
@@ -12,15 +9,13 @@ from core.services import ProductsService
 router = APIRouter(prefix=settings.api.v1.insulin, tags=["insulin"])
 
 
-@router.get("/products/insulin-dose/{{products}}/", response_model=InsulinDose)
+@router.get("/products/insulin-dose/{products}/", response_model=InsulinDose)
 async def get_product_by_name(
     request: Request,
     products: ProductList,
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-):
-    insulin_dose: dict[str, str] = ProductsService(
-        session=session
-    ).calculate_insulin_dose()
+    product_service: Annotated[ProductsService, Depends(ProductsService)],
+) -> InsulinDose | HTTPException:
+    insulin_dose: dict[str, str] = product_service.calculate_insulin_dose()
     return InsulinDose(**insulin_dose)
 
 
@@ -28,8 +23,6 @@ async def get_product_by_name(
 async def create_product(
     request: Request,
     product_creds: ProductCreate,
-    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-):
-    return await ProductsService(session=session).create_user_product(
-        product_creds=product_creds
-    )
+    product_service: Annotated[ProductsService, Depends(ProductsService)],
+) -> ProductInfo:
+    return await product_service.create_user_product(product_creds=product_creds)
