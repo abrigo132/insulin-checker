2d3a656ba671e3e6bb3ba2d10627b03d984a62d5
abrigo132 <garipotter97@gmail.com>
2025-05-26 10:52:48 +0500

    add product create + product info + product list + insulin dose pydantic schemas for export

--- src/core/schemas/__init__.py
+++ src/core/schemas/__init__.py
@@ -7,7 +7,12 @@ __all__ = (
     "Token",
     "TokenInfo",
     "UserRegisterCreds",
+    "ProductCreate",
+    "ProductInfo",
+    "ProductList",
+    "InsulinDose",
 )
 
 from .users import User, UserToken, UserRead, UserLogin, UserRegisterCreds, UserCreate
 from .jwt import Token, TokenInfo
+from .products import ProductCreate, ProductInfo, ProductList, InsulinDose
